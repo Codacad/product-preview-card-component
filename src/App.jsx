8c6079274d6a78e7e8eca74a5a7d74b186734345
Mohd Rizwan <iamrizwan40@gmail.com>
2023-11-26 17:04:57 +0530

    moblie responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="app h-[100vh] bg-appBackground flex flex-col justify-center items-center font-Fraunces">
-        <div className="product-review-component bg-reviewComponentBackground grid grid-cols-2 w-[40%] m-auto rounded-md">
+      <div className="app min-h-[100vh] bg-appBackground flex flex-col justify-center items-center font-Fraunces p-4 md:p-0">
+        <div className="product-review-component bg-reviewComponentBackground md:grid md:grid-cols-2 m-auto rounded-md min-h-[100vh]">
           <div className="img rounded-l-md">
-            <img src={ProductImage} className="rounded-l-md" alt="Product Image" />
+            <img src={ProductImage} className="rounded-l-md w-full h-full" alt="Product Image" />
           </div>
           <div className="text-content py-8 px-10 flex flex-col justify-between gap-4">
             <h3 className="font-sm font-Montserrat uppercase text-darkGrayishBlue tracking-[.2rem] font-semibold">Perfume</h3>
@@ -16,16 +16,16 @@ function App() {
               A floral, solar and voluptuous interpretation composed by Olivier
               Polge, Perfumer-Creator for the House of CHANEL.
             </p>
-            <div className="price flex items-center">
+            <div className="price grid grid-cols-2 items-center">
               <h2 className="text-darkCyan font-Montserrat text-2xl font-extrabold">$149.99</h2>
-              <span className="text-darkGrayishBlue line-through ml-6">$169.99</span>
+              <span className="text-darkGrayishBlue line-through flex justify-center"><span>$169.99</span></span>
             </div>
             <button className="bg-darkCyan text-white p-3 rounded-lg font-Montserrat font-bold">             
               <span className="flex justify-center text-sm"> <img className="mr-2" src={Cart} alt="Cart" /> Add to Cart</span>
             </button>
           </div>
         </div>
-        <div className="attribution">
+        <div className="attribution pt-4 md:pt-4">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io?ref=challenge">
             Frontend Mentor
